99b5b30236482ae930be860c8c9f509358715914
patel-udit-patel <udit0428t@gmail.com>
2025-02-23 20:31:26 +0530

    [Udit Patel] Add. Use MVC Controller to invoke a view

--- Hellodemo/src/main/java/com/example/Hellodemo/controller/HelloController.java
+++ Hellodemo/src/main/java/com/example/Hellodemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.Hellodemo.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+    //UC_02 (MVC - Thymeleaf)
+    @GetMapping("/UC2")
+    public String sayHelloUC2(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        //This must match a Thymeleaf template (hello.html)
+        return "hello";
+
     }
 }
